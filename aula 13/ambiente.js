d2bd44245c66afb450bc8c371d44ceb369a7fb15
Fernando Maciel <fernandonevesmaciel@gmail.com>
2025-04-28 21:08:29 -0300

    aula 13

---
+++ aula 13/ambiente.js
@@ -0,0 +1,29 @@
+// repetição com teste logico em baixo
+
+var c = 1 
+
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while( c <= 2)
+
+/* repetição com teste logico em cima
+
+var c = 1 
+
+while ( c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+/*
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/
