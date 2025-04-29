beb5f90d3233fecc2fccfe98fe00d441f54d30e5
Fernando Maciel <fernandonevesmaciel@gmail.com>
2025-04-29 18:59:56 -0300

    aula 14 exercicio 16

---
+++ aula 14/ambiente.js
@@ -0,0 +1,25 @@
+// exemplo usando repetição whille
+/*
+var c = 1 
+while(c <= 5){
+    console.log(c)
+    c++
+}
+*/
+
+// exemplo usando repetição do
+/*
+var c = 1
+
+do{
+    console.log(c)
+    c++
+}while(c <= 10)
+*/
+
+// exemplo usando repetição for
+console.log('Vai começar...')
+for(var c = 1;c <= 10;c++){
+    console.log(c)
+}
+console.log('FIM!')
